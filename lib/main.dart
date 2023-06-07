7b6a513f4d3b38045b4dae7df1d4ffdc7a69dbb7
Tanvir Ahmed Nayeem <62479601+Tanvir-AN@users.noreply.github.com>
2023-06-07 23:18:38 +0600

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Assignment App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    PageWidget(1, Colors.yellow),
+    PageWidget(2, Colors.blue),
+    PageWidget(3, Colors.black),
+  ];
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Assignment App'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 0;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Dashboard'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 1;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+            ListTile(
+              title: Text('Settings'),
+              onTap: () {
+                setState(() {
+                  _currentIndex = 2;
+                  Navigator.pop(context);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onPageChanged,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageWidget extends StatelessWidget {
+  final int pageNumber;
+  final Color color;
+
+  PageWidget(this.pageNumber, this.color);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: color,
+        child: Icon(Icons.add),
+      ),
+      body: Center(
+        child: Text(
+          'Page $pageNumber',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
